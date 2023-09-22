7557ac880554b710bd36d299b353dc1195626bf7
karine <gulkarine91@gmail.com>
2023-09-22 19:47:49 +0200

    chore: start work on navbar

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          {/* <Routes>
+
+          </Routes> */}
+        </main>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
