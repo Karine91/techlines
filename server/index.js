15f09e51f3eb0e0e0039ab7b8beefb7223c4fdd7
karine <gulkarine91@gmail.com>
2023-11-07 14:31:08 +0100

    chore: get products works

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import connectToDatabase from "./database.js";
 
 import express from "express";
 
+import productRoutes from "./routes/productRoutes.js";
+
 dotenv.config();
 connectToDatabase();
 const app = express();
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log("Server runs on port " + port);
